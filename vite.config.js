3045aeb2175078f2b9e21bee528e8e7c3a933644
jatin <jatin0700.be23@chitkara.edu.in>
2024-12-25 15:44:28 +0530

    Updated Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/MotifyX/' : '/', 
-})
+  base: process.env.NODE_ENV === 'production' ? '/MotifyX/' : '/', // Conditional base path
+});
